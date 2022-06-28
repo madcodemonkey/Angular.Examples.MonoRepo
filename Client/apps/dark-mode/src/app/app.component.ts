49488664036ad99e4a38a45f290e1af517035108
David Yates <yates.programmer@gmail.com>
2022-06-28 16:46:54 -0700

    Update the client

--- Client/apps/dark-mode/src/app/app.component.ts
+++ Client/apps/dark-mode/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'yates-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'dark-mode';
